67fa43921a6ea52648bc9195efc80fe3c693af87
iwallace <iw2000@wildcats.enh.edu>
2018-01-12 11:24:34 -0500

    add pace functionality

---
+++ home.html.php
@@ -0,0 +1,9 @@
+<h1>Welcome to the split calc</h1>
+<h4>To use, put in your desired finishing time.</h4>
+
+
+<form action="">
+<input type='text' name='hours' placeholder='hours'>
+<input type='text' name='minutes' placeholder='minutes'>
+<input type='submit' name='submit' value='submit'>
+</form> 
