18e38cb31fa80af55f8a60ab6f28e69a7fec16c0
iwallace <iw2000@wildcats.unh.edu>
2018-03-06 21:50:11 +0000

    pace working with seconds

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 
 if(isset($_REQUEST['hours'])){
 
-    echo 'hours: ' . $_REQUEST['hours'] . '<br>';
-    echo 'minutes: ' . $_REQUEST['minutes'] . '<br>';
-    echo 'seconds: ' . $_REQUEST['seconds'] . '<br>';
-
     // get pace, with "seconds" as percent 
-    $pace = ($_REQUEST['hours']*60*60+$_REQUEST['minutes']*60+$_REQUEST['seconds']);///13.1094;
-    echo '<br> pace: ' . $pace . '<br>';
+    $pace = ($_REQUEST['hours']*60*60+$_REQUEST['minutes']*60+$_REQUEST['seconds'])/13.1094;
 
+    $pace = $pace/60;
     // split pace on the dot
     $paces = explode('.', $pace );
     
@@ -17,12 +13,13 @@ if(isset($_REQUEST['hours'])){
     
     //convert seconds into seconds rather than a percent 
     $seconds = ('.'.substr($paces[1],0,2))*60; 
-    
     //add a leading 0 if seconds are single digit 
-    if(strlen($seconds) == 1){$seconds = '0'.$seconds;}
+    if(strlen($seconds) == 1){
+        $seconds = '0'.$seconds;
+    }
     
     // echo out pace 
-    $paceMsg =  $minutes.":".$seconds.' pace needed to run a half in '
+    $paceMsg =  '<br>' . $minutes.":".$seconds.' pace needed to run a half in '
          .$_REQUEST['hours']." hours and "
          .$_REQUEST['minutes']." minutes.";    
 
