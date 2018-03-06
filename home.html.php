fe22f502d91bd0d4180625aa721bbd76dc35e827
iwallace <iw2000@wildcats.unh.edu>
2018-03-06 21:18:59 +0000

    add seconds field

--- home.html.php
+++ home.html.php
@@ -5,5 +5,6 @@
 <form action="">
 <input type='text' name='hours' placeholder='hours'>
 <input type='text' name='minutes' placeholder='minutes'>
+<input type='text' name='seconds' placeholder='seconds'>
 <input type='submit' name='submit' value='submit'>
 </form> 
